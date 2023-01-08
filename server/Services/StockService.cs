e9481663c4fdd9bef4d103f88a3e442bf397a11b
AndrewLin95 <andrew.lin88@hotmail.com>
2023-01-08 13:48:32 -0500

    updated external api call using restsharp

--- server/Services/StockService.cs
+++ server/Services/StockService.cs
@@ -1,8 +1,6 @@
 using StockTraderNext.Models;
-using Microsoft.Extensions.Options;
 using RestSharp;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace StockTraderNext.Services;
 
@@ -12,22 +10,47 @@ public class StockService
 
   public StockService()
   {
-    _client = new RestClient("https://www.alphavantage.co/");
+    var url = "https://www.alphavantage.co/";
+    _client = new RestClient(url);
   }
 
-
-  public async Task<Stocks> GetStockAsync()
+  public async Task<String> GetStockAsync()
   {
+
     var request = new RestRequest("query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=QIBYL49PJ38VSZ0B");
+
     var response = await _client.ExecuteGetAsync(request);
 
-    if (!response.IsSuccessful)
-    {
-      Console.WriteLine(response);
-    }
+    Console.WriteLine(response.Content);
+    Console.Read();
 
-    var stockData = JsonSerializer.Deserialize<Stocks>(response.Content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-
-    return stockData;
+    return "test";
   }
+
+
+
+
+
+
+
+  // private readonly HttpClient _httpClient;
+
+  // public StockService(HttpClient httpClient)
+  // {
+  //   _httpClient = httpClient;
+  //   _httpClient.BaseAddress = new Uri("https://www.alphavantage.co/");
+  // }
+
+  // public async Task<String> GetStockAsync()
+  // {
+  //   var response = await _httpClient.GetAsync("query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=QIBYL49PJ38VSZ0B");
+
+  //   response.EnsureSuccessStatusCode();
+
+  //   using var responseStream = await response.Content.ReadAsStreamAsync();
+  //   var responseObject = await JsonSerializer.DeserializeAsync<Stocks>(responseStream);
+
+  //   Console.WriteLine(responseObject);
+  //   return "test";
+  // }
 }
