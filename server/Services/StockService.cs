8c773e22794918a9d6fd83c69f2a6d7a83afd5fa
AndrewLin95 <andrew.lin88@hotmail.com>
2023-01-11 16:52:14 -0500

    comments

--- server/Services/StockService.cs
+++ server/Services/StockService.cs
@@ -14,22 +14,21 @@ public class StockService
     _client = new RestClient(url);
   }
 
+  // returns the intraday stock data
   public async Task<StockDailyResponseData> GetStockAsync()
   {
-
+    // GET request to alpha vantage
     var request = new RestRequest("query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=QIBYL49PJ38VSZ0B");
-
     var response = await _client.ExecuteGetAsync(request);
-
-    // Console.WriteLine(response.Content);
-    // Console.Read();
-
+    // Deserializes the data to align with the Stocks model
     var responseData = JsonSerializer.Deserialize<Stocks>(response.Content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
+    // Only collects the stock prices if they are within trading hours (every 5 minutes)
     var stockValues = new List<StockData>();
 
     foreach (var item in responseData.timeSeries)
     {
+      // NOTE: alpha vantage's intra date values are -1 day. The below "current day" takes this into account
       var currentYear = Int32.Parse(DateTime.Now.ToString("yyyy"));
       var currentMonth = Int32.Parse(DateTime.Now.ToString("MM"));
       var currentDay = Int32.Parse(DateTime.Now.ToString("dd"));
@@ -41,6 +40,7 @@ public class StockService
       var compareLowResults = DateTime.Compare(parseTime, dateTimeLow);
       var compareHighResults = DateTime.Compare(parseTime, dateTimeHigh);
 
+      // result > 0 = after, result < 0 = before
       if (compareLowResults > 0 && compareHighResults < 0)
       {
         stockValues.Add(new StockData { stockPrice = item.Value._1Open, time = item.Key });
