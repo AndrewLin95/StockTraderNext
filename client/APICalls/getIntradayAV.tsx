078f2514d3a9459a8f192c592fefd3b0cb95b64a
AndrewLin95 <andrew.lin88@hotmail.com>
2023-01-12 16:28:12 -0500

    created an array to retrieve info instead

--- client/APICalls/getIntradayAV.tsx
+++ client/APICalls/getIntradayAV.tsx
@@ -1,22 +1,32 @@
 import { StockDailyResponseData } from '../utils/Models';
 
-export default async function getIntradayAV(stockSymbol: string) {
-  const url = `/api/stock/${stockSymbol}`;
+export default async function getIntradayAV(stockSymbol: string[]) {
+  const arrayLength = stockSymbol.length;
+  console.log(arrayLength);
 
-  const requestOptions = {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
+  const stockData: StockDailyResponseData[] = [];
+
+  let i = 0;
+  while (i < arrayLength) {
+    const url = `/api/stock/${stockSymbol[i]}`;
+
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const response = await fetch(url, requestOptions);
+      const data: StockDailyResponseData = await response.json();
+      stockData.push(data);
+    } catch (err) {
+      throw err;
     }
-  };
-
-  try {
-    const response = await fetch(url, requestOptions);
-    const data: StockDailyResponseData = await response.json();
-    console.log(data);
-    return data;
-  } catch (err) {
-    throw err;
+    i++;
   }
+
+  return stockData;
 }
--- client/pages/stocks/Details.tsx
+++ client/pages/stocks/Details.tsx
@@ -4,15 +4,15 @@ import getIntradayAV from '../../APICalls/getIntradayAV';
 import { StockDailyResponseData } from '../../utils/Models';
 
 const Details = () => {
-  const intraDayDataInterface = {} as StockDailyResponseData;
-  const [intraDayData, setIntraDayData] = useState<StockDailyResponseData>(
+  const intraDayDataInterface = {} as StockDailyResponseData[];
+  const [intraDayData, setIntraDayData] = useState<StockDailyResponseData[]>(
     intraDayDataInterface
   );
   const [loading, setLoading] = useState(true);
 
   // API call to .net Server
   async function getIntradayData() {
-    const _data = await getIntradayAV('ibm');
+    const _data = await getIntradayAV(['ibm', 'intc']);
     setIntraDayData(_data);
     setLoading(false);
   }
@@ -30,10 +30,14 @@ const Details = () => {
   }
 
   return (
-    <div className="border border-yellow-500 w-full h-full">
+    <div className="border border-yellow-500 w-full h-full overflow-auto">
       <div>Static stock tracker info</div>
       <div>main page contents</div>
-      <LineChart intraDayData={intraDayData} />
+      <>
+        {Object.entries(intraDayData).map(([key, value]) => {
+          return <LineChart key={key} intraDayData={value} />;
+        })}
+      </>
     </div>
   );
 };
